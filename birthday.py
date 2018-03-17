afc5782c976eaa82e3d60f4222da4b00ab9a5e9a
Prakash Acharya <33595598+mirmire@users.noreply.github.com>
2018-03-17 11:21:07 +0200

    ex2. added important birthdays

--- birthday.py
+++ birthday.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python2
 from iso_date import DateDistance, ISODate
-my_birthdate = ISODate(1994, 11, 03)
+birthdate = ISODate(1994, 11, 03)
 date_now = ISODate()
 
+distance_between = birthdate.get_distance_to(date_now)
 
-distance_between = my_birthdate.get_distance_to(date_now)
-
-
-
-print "\n   I am %d years, %d months, and %d days" \
+print "\nYou are %d years, %d months, and %d days" \
       " old.\n"  %  ( distance_between.years,
       distance_between.months, distance_between.days )
+print "Your birth week was %d" % birthdate.get_week_number()
+
+years_to_celebrate = 10
+
+while years_to_celebrate < 80:
+    date_to_celebrate = ISODate(birthdate.year() + years_to_celebrate,
+                                birthdate.month(),
+                                birthdate.day())
+    print "\n %d years old on %s (%s)" % (years_to_celebrate,
+          date_to_celebrate, date_to_celebrate.get_day_of_week()),
+
+    years_to_celebrate += 10
